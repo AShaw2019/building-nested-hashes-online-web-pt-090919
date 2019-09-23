b72ffec5c6b7998ba1918b4b43a5d6949dc15493
Ashley Twomey <aprier091012@gmail.com>
2019-09-23 00:34:43 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -21,7 +21,10 @@ def fourth_challenge
 }
 
 end
-epic_tragedy[:montague][:hero][:status] << "dead"
-epic_tragedy[:capulet][:heroine][:status] << "dead"
+epic_tragedy[:montague][:hero][:status] = "dead"
 puts epic_tragedy
+end
+epic_tragedy[:capulet][:heroine][:status] = "dead"
+puts epic_tragedy
+end
 end
